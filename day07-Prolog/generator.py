1f9e7adf156c6e1c2dd97b0101d8ed4df2d39b95
Kåre <kare.kvg@gmail.com>
2022-03-29 16:58:52 +0200

    Finished part 1 by geberating Prolog code.
    This is my first time writing logical programming code and it is
    probably shit. But I got something working, even if it would be nice
    to do in a prettier way.

---
+++ day07-Prolog/generator.py
@@ -0,0 +1,28 @@
+import sys
+import re
+
+
+def main():
+    lines = sys.stdin.readlines()
+
+    leaf_regex = re.compile(r"(\w+) \(\d+\)")
+    node_regex = re.compile(r"(\w+) \(\d+\) -> ((?:\w+(?:, )?)+)")
+
+    carried_map = {}
+
+    for line in lines:
+        match = node_regex.match(line)
+        if match:
+            for carried in match.group(2).split(", "):
+                carried_map[carried] = match.group(1)
+    
+    with open("gen.pl", 'w') as f:
+        for (carried, carrier) in carried_map.items():
+            f.write(f"carries({carrier}, {carried}).\n")
+
+        f.write("\nbottomCarrier(X) :- writeln(X), carries(Y, X), bottomCarrier(Y).\n")
+        f.write(f"\nresult :- bottomCarrier({carried}).\n")
+
+
+if __name__ == "__main__":
+    main()
