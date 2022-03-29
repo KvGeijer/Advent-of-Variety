49d0473cbe4aff388a8d18f27385d6e52b05bc14
Kåre <kare.kvg@gmail.com>
2022-03-29 17:01:06 +0200

    Added hand written prolog file for the example
    This is the one I create the strategy for and make sure it works

---
+++ day07-Prolog/example.pl
@@ -0,0 +1,19 @@
+% The idea is to just pick one random program and then follow the path up to find the one carrying it not being carried
+carries(fwft, ktlj).
+carries(fwft, cntj).
+carries(fwft, xhth).
+
+carries(padx, pbga).
+carries(padx, havc).
+carries(padx, qoyq).
+
+carries(tknk, ugml).
+carries(tknk, padx).
+carries(tknk, fwft).
+
+carries(ugml, gyxo).
+carries(ugml, ebii).
+carries(ugml, jptl).
+
+% This writes out all the carriers until it can't match no more. It really is not that nice...
+bottomCarrier(X) :- writeln(X), carries(Y, X), bottomCarrier(Y).
